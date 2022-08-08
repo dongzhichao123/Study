5723e887ecd3d9d6fb3419a72c09288296737c78
dongzhichao123 <2939937011@qq.com>
2022-08-08 22:03:27 +0800

    git testtwo

--- Study/springboot_demo/src/main/java/com/example/springboot_demo/rabbitmq/two/RabbitMQAck.java
+++ Study/springboot_demo/src/main/java/com/example/springboot_demo/rabbitmq/two/RabbitMQAck.java
@@ -16,7 +16,6 @@ public class RabbitMQAck {
     public void TestAckOne(String mag, Channel channel, Message message) throws IOException {
         try {
             System.out.println("TestACK.ONE" + mag);
-//            int i = 1 / 0;
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         } catch (Exception e) {
             e.printStackTrace();
